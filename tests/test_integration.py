aca3d34c6bfa5766d7a31ce32eec2e9b9779a53b
Salim Hamed <salimhamed@gmail.com>
2024-03-06 09:31:25 -0800

    fix enum

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -20,14 +20,16 @@ def test_integration():
     filter_options = FilterOptions(min_monthly_search_volume_exact=150)
 
     keywords_by_asin = client.keywords_by_asin(
-        asin=["B005IHSKYS", "B0CL5KNB9M"], filter_options=filter_options, sort_option=Sort.monthly_search_volume_exact
+        asin=["B005IHSKYS", "B0CL5KNB9M"],
+        filter_options=filter_options,
+        sort_option=Sort.MONTHLY_SEARCH_VOLUME_EXACT_MATCH,
     )
 
     keywords_by_keyword = client.keywords_by_keyword(
         search_terms="yoga",
         categories=["Home & Kitchen", "Musical Instruments"],
         marketplace=Marketplace.US,
-        sort_option=Sort.monthly_trend,
+        sort_option=Sort.MONTHLY_TREND,
     )
 
     for keyword in keywords_by_asin[:10]:
