81c309a1aad14cfa1e182001234dca3388837745
Salim Hamed <salimhamed@gmail.com>
2024-03-11 12:05:59 -0700

    initial commit of docs

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "jungle-scout"
+copyright = "2024, Jungle Scout <support@junglescout.com>"
+author = "Jungle Scout <support@junglescout.com>"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. jungle-scout documentation master file, created by
+   sphinx-quickstart on Mon Mar 11 11:29:22 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to jungle-scout's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. autoclass:: jungle_scout.client.Client
+   :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
