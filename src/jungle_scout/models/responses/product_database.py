1e1258c313186b08b6faab2732513f1b266fba89
Salim Hamed <salimhamed@gmail.com>
2024-03-18 10:56:50 -0700

    add test

--- src/jungle_scout/models/responses/product_database.py
+++ src/jungle_scout/models/responses/product_database.py
@@ -6,17 +6,17 @@ from pydantic import BaseModel, Field
 class SubcategoryRanks(BaseModel):
     """Represents a response object containing subcategory ranks."""
 
-    subcategory: str = Field(default=..., description="")
-    rank: str = Field(default=..., description="")
+    subcategory: str = Field(default=..., description="The subcategory of the product.")
+    rank: int = Field(default=..., description="The rank of the product.")
 
 
 class FeeBreakdown(BaseModel):
     """Represents a response object containing fee breakdown."""
 
-    fba_fee: int = Field(default=..., description="")
-    referral_fee: int = Field(default=..., description="")
-    variable_closing_fee: int = Field(default=..., description="")
-    total_fees: int = Field(default=..., description="")
+    fba_fee: int = Field(default=..., description="The FBA fee of the product.")
+    referral_fee: int = Field(default=..., description="The referral fee of the product.")
+    variable_closing_fee: int = Field(default=..., description="The variable closing fee of the product.")
+    total_fees: int = Field(default=..., description="The total fees of the product.")
 
 
 class ProductDatabaseAttributes(BaseModel):
@@ -32,30 +32,30 @@ class ProductDatabaseAttributes(BaseModel):
     is_variant: bool = Field(default=..., description="Whether the product is a variant.")
     seller_type: str = Field(default=..., description="The type of the seller.")
     variants: str = Field(default=..., description="The variants of the product.")
-    is_standalone: str = Field(default=..., description="Whether the product is standalone.")
-    is_parent: str = Field(default=..., description="Whether the product is a parent.")
+    is_standalone: bool = Field(default=..., description="Whether the product is standalone.")
+    is_parent: bool = Field(default=..., description="Whether the product is a parent.")
     brand: str = Field(default=..., description="The brand of the product.")
-    product_rank: str = Field(default=..., description="The rank of the product.")
-    weight_value: str = Field(default=..., description="The weight value of the product.")
-    weight_unit: str = Field(default=..., description="The weight unit of the product.")
-    length_value: str = Field(default=..., description="The length value of the product.")
-    width_value: str = Field(default=..., description="The width value of the product.")
-    height_value: str = Field(default=..., description="The height value of the product.")
-    dimensions_unit: str = Field(default=..., description="The dimensions unit of the product.")
-    listing_quality_score: str = Field(default=..., description="The listing quality score of the product.")
-    number_of_sellers: str = Field(default=..., description="The number of sellers for the product.")
+    product_rank: int = Field(default=..., description="The rank of the product.")
+    weight_value: int = Field(default=..., description="The weight value of the product.")
+    weight_unit: int = Field(default=..., description="The weight unit of the product.")
+    length_value: int = Field(default=..., description="The length value of the product.")
+    width_value: int = Field(default=..., description="The width value of the product.")
+    height_value: int = Field(default=..., description="The height value of the product.")
+    dimensions_unit: int = Field(default=..., description="The dimensions unit of the product.")
+    listing_quality_score: int = Field(default=..., description="The listing quality score of the product.")
+    number_of_sellers: int = Field(default=..., description="The number of sellers for the product.")
     buy_box_owner: str = Field(default=..., description="The buy box owner of the product.")
     buy_box_owner_seller_id: str = Field(default=..., description="The buy box owner seller ID of the product.")
-    date_first_available: str = Field(default=..., description="The date the product was first available.")
-    date_first_available_is_estimated: str = Field(
+    date_first_available: datetime = Field(default=..., description="The date the product was first available.")
+    date_first_available_is_estimated: bool = Field(
         default=..., description="Whether the date the product was first available is estimated."
     )
-    approximate_30_day_revenue: str = Field(default=..., description="The approximate 30 day revenue of the product.")
-    approximate_30_day_units_sold: str = Field(
+    approximate_30_day_revenue: int = Field(default=..., description="The approximate 30 day revenue of the product.")
+    approximate_30_day_units_sold: int = Field(
         default=..., description="The approximate 30 day units sold of the product."
     )
     ean_list: str = Field(default=..., description="The EAN list of the product.")
-    variant_reviews: str = Field(default=..., description="The variant reviews of the product.")
+    variant_reviews: int = Field(default=..., description="The variant reviews of the product.")
     updated_at: datetime = Field(default=..., description="The date the product was last updated.")
     subcategory_ranks: SubcategoryRanks = Field(default=..., description="The subcategory ranks of the product.")
     fee_breakdown: FeeBreakdown = Field(default=..., description="The fee breakdown of the product.")
---
+++ tests/models/responses/test_product_database.py
@@ -0,0 +1,56 @@
+import json
+
+from datetime import datetime
+from jungle_scout.models.responses.product_database import ProductDatabase
+
+
+def test_product_database():
+    response_obj = ProductDatabase.model_validate_json(
+        json.dumps(
+            {
+                "links": "links",
+                "meta": "meta",
+                "id": "123",
+                "type": "foo",
+                "attributes": {
+                    "title": "foo",
+                    "price": 1,
+                    "reviews": 1,
+                    "category": "foo",
+                    "rating": "foo",
+                    "image_url": "foo",
+                    "parent_asin": "foo",
+                    "is_variant": True,
+                    "seller_type": "foo",
+                    "variants": "foo",
+                    "is_standalone": True,
+                    "is_parent": True,
+                    "brand": "foo",
+                    "product_rank": 1,
+                    "weight_value": 1,
+                    "weight_unit": 1,
+                    "length_value": 1,
+                    "width_value": 1,
+                    "height_value": 1,
+                    "dimensions_unit": 1,
+                    "listing_quality_score": 1,
+                    "number_of_sellers": 1,
+                    "buy_box_owner": "foo",
+                    "buy_box_owner_seller_id": "foo",
+                    "date_first_available": datetime(year=2023, month=10, day=5).strftime("%Y-%m-%d"),
+                    "date_first_available_is_estimated": True,
+                    "approximate_30_day_revenue": 1,
+                    "approximate_30_day_units_sold": 1,
+                    "ean_list": "foo",
+                    "variant_reviews": 1,
+                    "updated_at": datetime(year=2023, month=10, day=5).strftime("%Y-%m-%d"),
+                    "subcategory_ranks": {"subcategory": "foo", "rank": 1},
+                    "fee_breakdown": {"fba_fee": 1, "referral_fee": 1, "variable_closing_fee": 1, "total_fees": 1},
+                },
+            }
+        )
+    )
+    assert isinstance(response_obj, ProductDatabase)
+    assert isinstance(response_obj.attributes.date_first_available, datetime)
+    assert isinstance(response_obj.attributes.updated_at, datetime)
+    assert isinstance(response_obj.attributes.is_parent, bool)
