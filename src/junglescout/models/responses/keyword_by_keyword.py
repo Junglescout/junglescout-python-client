debfc2f0e0e8bf8754db849b111402da9b56d545
Paulo Xavier <contato.pauloxavier@gmail.com>
2024-03-19 16:47:38 -0300

    Add more typings for keyword_by_keyword

--- src/junglescout/models/responses/keyword_by_keyword.py
+++ src/junglescout/models/responses/keyword_by_keyword.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
@@ -7,16 +9,20 @@ class KeywordByKeywordAttributes(BaseModel):
     country: str = Field(default=..., description="The country of the keyword.")
     name: str = Field(default=..., description="The name of the keyword.")
     monthly_trend: int = Field(default=..., description="The monthly trend of the keyword.")
-    monthly_search_volume_exact: int = Field(default=..., description="The monthly search volume exact of the keyword.")
-    quarterly_trend: int = Field(default=..., description="The quarterly trend of the keyword.")
-    monthly_search_volume_broad: int = Field(default=..., description="The monthly search volume broad of the keyword.")
+    monthly_search_volume_exact: Optional[int] = Field(
+        default=None, description="The monthly search volume exact of the keyword."
+    )
+    quarterly_trend: Optional[float] = Field(default=None, description="The quarterly trend of the keyword.")
+    monthly_search_volume_broad: Optional[int] = Field(
+        default=None, description="The monthly search volume broad of the keyword."
+    )
     dominant_category: str = Field(default=..., description="The dominant category of the keyword.")
-    recommended_promotions: str = Field(default=..., description="The recommended promotions of the keyword.")
-    sp_brand_ad_bid: int = Field(default=..., description="The SP brand ad bid of the keyword.")
-    ppc_bid_broad: int = Field(default=..., description="The PPC bid broad of the keyword.")
-    ppc_bid_exact: int = Field(default=..., description="The PPC bid exact of the keyword.")
+    recommended_promotions: Optional[int] = Field(default=..., description="The recommended promotions of the keyword.")
+    sp_brand_ad_bid: Optional[int] = Field(default=None, description="The SP brand ad bid of the keyword.")
+    ppc_bid_broad: Optional[float] = Field(default=None, description="The PPC bid broad of the keyword.")
+    ppc_bid_exact: Optional[float] = Field(default=None, description="The PPC bid exact of the keyword.")
     ease_of_ranking_score: int = Field(default=..., description="The ease of ranking score of the keyword.")
-    relevancy_score: int = Field(default=..., description="The relevancy score of the keyword.")
+    relevancy_score: Optional[int] = Field(default=None, description="The relevancy score of the keyword.")
     organic_product_count: int = Field(default=..., description="The organic product count of the keyword.")
     sponsored_product_count: int = Field(default=..., description="The sponsored product count of the keyword.")
 
