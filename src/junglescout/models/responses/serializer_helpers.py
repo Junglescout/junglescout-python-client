31a1a46402a1206049a05f494a693d1545dcf17e
Paulo Roberto Xavier <contato.pauloxavier@gmail.com>
2024-10-07 11:00:59 -0300

    Adds optional on datetime functions

--- src/junglescout/models/responses/serializer_helpers.py
+++ src/junglescout/models/responses/serializer_helpers.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from typing import Optional
 
 
-def serialize_datetime(dt: datetime) -> str:
+def serialize_datetime(dt: Optional[datetime]) -> str:
     """Serialize a datetime object to a datetime string."""
-    return dt.strftime("%Y-%m-%dT%H:%M:%S.%f")
+    return dt.strftime("%Y-%m-%dT%H:%M:%S.%f") if dt else None
 
 
-def serialize_date(dt: datetime) -> str:
+def serialize_date(dt: Optional[datetime]) -> str:
     """Serialize a datetime object to a date string."""
-    return dt.strftime("%Y-%m-%d")
+    return dt.strftime("%Y-%m-%d") if dt else None
