c4479ea54a6d7127f851498d37ebdd9bc6cb887d
semantic-release <semantic-release>
2024-11-08 19:42:30 +0000

    0.3.0
    Automatically generated by python-semantic-release

--- src/junglescout/__init__.py
+++ src/junglescout/__init__.py
@@ -4,7 +4,7 @@ This package contains the Jungle Scout Client package.
 """
 
 __all__ = ["Client", "ClientAsync", "ClientSync"]
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 
 from .client_async import ClientAsync
 from .client_sync import ClientSync
