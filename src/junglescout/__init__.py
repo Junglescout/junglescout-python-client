3ab90e6c680fc50c974e2053009af956ba93fed1
Paulo Roberto Xavier <contato.pauloxavier@gmail.com>
2024-11-13 15:47:54 -0300

    chore: Manually bump version

--- src/junglescout/__init__.py
+++ src/junglescout/__init__.py
@@ -4,7 +4,7 @@ This package contains the Jungle Scout Client package.
 """
 
 __all__ = ["Client", "ClientAsync", "ClientSync", "__version__"]
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 from .client_async import ClientAsync
 from .client_sync import ClientSync
